0c8553118a79a3fb1e4a80064e97b23d5faf6882
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-05 15:18:51 -0400

    Added tests for time of each function

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,6 @@
+import time
+
+
 def fibonacci(n):
     nums = [0, 1]
     if n == 0:
@@ -14,3 +17,16 @@ def r_fibonacci(n):
         return 1
     else:
         return r_fibonacci(n - 1) + r_fibonacci(n - 2)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(fibonacci(20))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Non-recursie: {elapsed_time} seconds")
+    start_time = time.time()
+    print(r_fibonacci(20))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Recursive: {elapsed_time} seconds")
