ecd9c736a012ac0440469f96243344f62c585a6f
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-05 15:14:19 -0400

    Added recursive and non-recursive solution

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,16 @@
 def fibonacci(n):
-  pass
+    nums = [0, 1]
+    if n == 0:
+        return nums[0]
+    for i in range(1, n):
+        nums.append(nums[-1] + nums[-2])
+    return nums[-1]
 
 
+def r_fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return r_fibonacci(n - 1) + r_fibonacci(n - 2)
